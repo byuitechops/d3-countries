d6f52708fad9f6791003c6b026a0d6021370e1a2
Didymus <didymus.benson@gmail.com>
2018-03-22 09:16:14 -0600

    updated country data - Africa fixed.

--- script2.js
+++ script2.js
@@ -1,25 +1,16 @@
-// DECLARE CONTINENT FROM QUERY STRING
-var view = window.location.href.split("?")[1];
-
-
+// GRAB THE CONTINENT NAME FOR SORTING LATER.
+var continent = document.querySelector("#theMap").dataset.continent;
 var countries = {};
 
 var getCountries = new XMLHttpRequest();
 getCountries.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-
         d3.csv.parse(this.responseText).forEach(function (country) {
-            Object.keys(country).forEach(function (key) {
-                if (country[key].indexOf('\n') > 0)
-                    country[key] = country[key].split("\n");
+                if (country.Continent === continent) {
+                    countries[country.id] = country;
+                }
             })
-            countries[country.Code] = country;
-        })
-
-
-        console.log(countries);
-
-
+            //        console.log(countries);
         addListeners();
     }
 };
@@ -28,63 +19,51 @@ getCountries.send();
 
 
 
-
-function hideThem(callback) {
-    document.querySelectorAll(".datamaps-subunit").forEach(function (country) {
-        country.classList.add("hidden");
-    });
-    unHideThem();
-}
-
-function unHideThem(callback) {
-    continents[continent].codes.forEach(function (code) {
-        //    console.log(code);
-        if (document.querySelector("#Sudan"))
-            document.querySelector("#Sudan").classList.remove("hidden");
-    })
-}
-
 function addListeners() {
-
-
-    document.querySelectorAll("#Sudan path, #Sudan text").forEach(function (child) {
+    var selector = "";
+    Object.keys(countries).forEach(function (country) {
+        selector = "#" + countries[country].id + " path, #" + countries[country].id + " text";
+        console.log(selector)
+        document.querySelectorAll(selector).forEach(function (child) {
             child.addEventListener('click', function (event) {
-                createpopup(countries["SDN"]);
+                createpopup(countries[country].id);
                 event.preventDefault();
             }, false);
-           child.addEventListener("touchend", function (event) {
-                createpopup("Sudan");
+            child.addEventListener("touchend", function (event) {
+                createpopup(countries[country].id);
                 event.preventDefault();
             }, false);
 
-    });
+        });
+    })
 
 
-        document.querySelector("#popup").addEventListener("touchend", function (event) {
-            this.classList.add("hidden");
-            event.preventDefault();
-        }, false); document.querySelector("#popup").addEventListener("click", function (event) {
-            this.classList.add("hidden");
-            event.preventDefault();
-        }, false);
 
-        document.querySelector("#popup_insides").addEventListener('click', cancel_cancel); document.querySelector("#popup_insides").addEventListener("touchend", cancel_cancel);
+
+    document.querySelector("#popup").addEventListener("touchend", function (event) {
+        this.classList.add("hidden");
+        event.preventDefault();
+    }, false);
+    document.querySelector("#popup").addEventListener("click", function (event) {
+        this.classList.add("hidden");
+        event.preventDefault();
+    }, false);
+    document.querySelector("#popup_insides").addEventListener('click', cancel_cancel);
+    document.querySelector("#popup_insides").addEventListener("touchend", cancel_cancel);
+}
+
+var template = Handlebars.compile(document.querySelector('#country_popup').innerHTML);
+
+function cancel_cancel(event) {
+    event.stopPropagation();
+}
+
+function createpopup(code) {
+    var popup = document.querySelector("#popup_insides");
+    //        console.log(countries[code]);
+    var country = countries[code];
+    if (popup.parentElement.classList.contains("hidden")) {
+        popup.parentElement.classList.remove("hidden");
     }
-
-    var template = Handlebars.compile(document.querySelector('#country_popup').innerHTML);
-
-
-    function cancel_cancel(event) {
-        event.stopPropagation();
-    }
-
-
-    function createpopup(code) {
-        var popup = document.querySelector("#popup_insides");
-        console.log("Sudan");
-        var country = countries["SDN"];
-        if (popup.parentElement.classList.contains("hidden")) {
-            popup.parentElement.classList.remove("hidden");
-        }
-        popup.innerHTML = template(country);
-    }
+    popup.innerHTML = template(country);
+}
