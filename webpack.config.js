0ddd89c475b994700bd479324b04cdd8d8b6efe1
Aaron Shiffler <ajshiffler@gmail.com>
2018-06-19 19:14:30 -0600

    added 3 versions of bundle.js for comparison. Remove these later.

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,27 @@
 module.exports = {
-  mode:'production',
-  entry:'./src/script.js',
-  output:{
-    filename:'bundle.js',
-    path:__dirname+'/dist'
+  mode: 'development', // The three comments below are about mode:
+  // 'development' seemed to give a more readable version. 
+  // 'production' gives a minimized version
+  // 'none' defaults to development
+  entry: './src/script.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/dist'
   },
-  module:{
-    rules:[
-      { test: /\.hbs$/, loader: "handlebars-loader" },
-      { test: /\.js$/, loader:'babel-loader', exclude: /node_modules/ },
-      { test: /\.csv$/, loader:'dsv-loader' }
+  module: {
+    rules: [{
+        test: /\.hbs$/,
+        loader: "handlebars-loader"
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.csv$/,
+        loader: 'dsv-loader'
+      }
     ]
   }
 }
