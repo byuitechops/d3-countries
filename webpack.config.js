a856815dc57299e72ca146ae14245150353449cb
Shiffler <ajshiff@byui.edu>
2018-06-20 14:32:36 -0600

    Add position-responsive gray links to bottom of all 5 maps, linking to 3rdPartyLicense.html.

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,17 @@ module.exports = {
   mode: 'development', // The three comments below are about mode:
   // 'development' seemed to give a more readable version. 
   // 'production' gives a minimized version
-  // 'none' defaults to development
+  // 'none' seems like a hybrid. It's weird
   entry: './src/script.js',
   output: {
     filename: 'bundle.js',
     path: __dirname + '/dist'
   },
   module: {
-    rules: [{
-        test: /\.hbs$/,
-        loader: "handlebars-loader"
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.csv$/,
-        loader: 'dsv-loader'
-      }
+    rules: [
+      {test: /\.hbs$/, loader: "handlebars-loader"},
+      {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
+      {test: /\.csv$/, loader: 'dsv-loader'}
     ]
   }
 }
